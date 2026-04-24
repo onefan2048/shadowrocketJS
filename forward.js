2e52850938f94909c008defb05a7ead725a78e06
onefan2048 <onefan2048@gmail.com>
2026-04-25 00:12:01 +0800

    Add forward.js for request and response handling

---
+++ forward.js
@@ -0,0 +1,256 @@
+const _scriptSonverterCompatibilityType = typeof $response !== 'undefined' ? 'response' : typeof $request !== 'undefined' ? 'request' : ''
+const _scriptSonverterCompatibilityDone = $done
+try {
+  
+// 转换时间: 2026/4/25 00:04:30
+// 兼容性转换
+if (typeof $request !== 'undefined') {
+  const lowerCaseRequestHeaders = Object.fromEntries(
+    Object.entries($request.headers).map(([k, v]) => [k.toLowerCase(), v])
+  );
+
+  $request.headers = new Proxy(lowerCaseRequestHeaders, {
+    get: function (target, propKey, receiver) {
+      return Reflect.get(target, propKey.toLowerCase(), receiver);
+    },
+    set: function (target, propKey, value, receiver) {
+      return Reflect.set(target, propKey.toLowerCase(), value, receiver);
+    },
+  });
+}
+if (typeof $response !== 'undefined') {
+  const lowerCaseResponseHeaders = Object.fromEntries(
+    Object.entries($response.headers).map(([k, v]) => [k.toLowerCase(), v])
+  );
+
+  $response.headers = new Proxy(lowerCaseResponseHeaders, {
+    get: function (target, propKey, receiver) {
+      return Reflect.get(target, propKey.toLowerCase(), receiver);
+    },
+    set: function (target, propKey, value, receiver) {
+      return Reflect.set(target, propKey.toLowerCase(), value, receiver);
+    },
+  });
+}
+Object.getOwnPropertyNames($httpClient).forEach(method => {
+  if(typeof $httpClient[method] === 'function') {
+    $httpClient[method] = new Proxy($httpClient[method], {
+      apply: (target, ctx, args) => {
+        for (let field in args?.[0]?.headers) {
+          if (['host'].includes(field.toLowerCase())) {
+            delete args[0].headers[field];
+          } else if (['number'].includes(typeof args[0].headers[field])) {
+            args[0].headers[field] = args[0].headers[field].toString();
+          }
+        }
+        return Reflect.apply(target, ctx, args);
+      }
+    });
+  }
+})
+
+
+// QX 相关
+var setInterval = () => {}
+var clearInterval = () => {}
+var $task = {
+  fetch: url => {
+    return new Promise((resolve, reject) => {
+      if (url.method == 'POST') {
+        $httpClient.post(url, (error, response, data) => {
+          if (response) {
+            response.body = data
+            resolve(response, {
+              error: error,
+            })
+          } else {
+            resolve(null, {
+              error: error,
+            })
+          }
+        })
+      } else {
+        $httpClient.get(url, (error, response, data) => {
+          if (response) {
+            response.body = data
+            resolve(response, {
+              error: error,
+            })
+          } else {
+            resolve(null, {
+              error: error,
+            })
+          }
+        })
+      }
+    })
+  },
+}
+
+var $prefs = {
+  removeValueForKey: key => {
+    let result
+    try {
+      result = $persistentStore.write('', key)
+    } catch (e) {
+    }
+    if ($persistentStore.read(key) == null) return result
+    try {
+      result = $persistentStore.write(null, key)
+    } catch (e) {
+    }
+    if ($persistentStore.read(key) == null) return result
+    const err = '无法模拟 removeValueForKey 删除 key: ' + key
+    console.log(err)
+    $notification.post('Script Hub: 脚本转换', '❌ qx-response.js.txt', err)
+    return result
+  },
+  valueForKey: key => {
+    return $persistentStore.read(key)
+  },
+  setValueForKey: (val, key) => {
+    return $persistentStore.write(val, key)
+  },
+}
+
+var $notify = (title = '', subt = '', desc = '', opts) => {
+  const toEnvOpts = (rawopts) => {
+    if (!rawopts) return rawopts 
+    if (typeof rawopts === 'string') {
+      if ('undefined' !== typeof $loon) return rawopts
+      else if('undefined' !== typeof $rocket) return rawopts
+      else return { url: rawopts }
+    } else if (typeof rawopts === 'object') {
+      if ('undefined' !== typeof $loon) {
+        let openUrl = rawopts.openUrl || rawopts.url || rawopts['open-url']
+        let mediaUrl = rawopts.mediaUrl || rawopts['media-url']
+        return { openUrl, mediaUrl }
+      } else {
+        let openUrl = rawopts.url || rawopts.openUrl || rawopts['open-url']
+        if('undefined' !== typeof $rocket) return openUrl
+        return { url: openUrl }
+      }
+    } else {
+      return undefined
+    }
+  }
+  console.log(title, subt, desc, toEnvOpts(opts))
+  $notification.post(title, subt, desc, toEnvOpts(opts))
+}
+var _scriptSonverterOriginalDone = $done
+var _scriptSonverterDone = (val = {}) => {
+  let result
+  if (
+    (typeof $request !== 'undefined' &&
+    typeof val === 'object' &&
+    typeof val.status !== 'undefined' &&
+    typeof val.headers !== 'undefined' &&
+    typeof val.body !== 'undefined') || false
+  ) {
+    try {
+      for (const part of val?.status?.split(' ')) {
+        const statusCode = parseInt(part, 10)
+        if (!isNaN(statusCode)) {
+          val.status = statusCode
+          break
+        }
+      }
+    } catch (e) {}
+    if (!val.status) {
+      val.status = 200
+    }
+    if (!val.headers) {
+      val.headers = {
+        'Content-Type': 'text/plain; charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST,GET,OPTIONS,PUT,DELETE',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      }
+    }
+    result = { response: val }
+  } else {
+    result = val
+  }
+  console.log('$done')
+  try {
+    console.log(JSON.stringify(result))
+  } catch (e) {
+    console.log(result)
+  }
+  _scriptSonverterOriginalDone(result)
+}
+var window = globalThis
+window.$done = _scriptSonverterDone
+var global = globalThis
+global.$done = _scriptSonverterDone
+
+/*
+ * fw
+ * 
+[rewrite_local]
+^https://fluxapi\.vvebo\.vip/v1/purchase/iap/subscription url script-analyze-echo-response https://mock.forward1.workers.dev/forward/qx-response.js
+
+[mitm]
+hostname = fluxapi.vvebo.vip
+ */
+
+const StatusTexts = {
+  200: "OK", 201: "Created", 202: "Accepted", 204: "No Content",
+  400: "Bad Request", 401: "Unauthorized", 403: "Forbidden", 404: "Not Found",
+  500: "Internal Server Error", 502: "Bad Gateway", 503: "Service Unavailable"
+};
+
+const requestUrl = $request.url;
+const requestHeaders = $request.headers;
+const requestBody = $request.body;
+
+const options = {
+  url: "https://mock.forward1.workers.dev/forward/v1/purchase/iap/subscription",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json; charset=utf-8",
+    "X-Auth-Key": requestHeaders["x-auth-key"] || ""
+  },
+  body: requestBody,
+  timeout: 10000
+};
+
+$task.fetch(options).then(
+  response => {
+    _scriptSonverterDone({
+      status: `HTTP/1.1 ${response.statusCode || 200} ${StatusTexts[response.statusCode || 200]}`,
+      headers: response.headers || {
+        server: "openresty",
+        date: new Date().toUTCString(),
+        "content-type": "application/json; charset=utf-8",
+      },
+      body: response.body
+    });
+  },
+  reason => {
+    console.log("Request failed:", reason);
+    _scriptSonverterDone({
+      status: "HTTP/1.1 500 Internal Server Error",
+      headers: {
+        server: "openresty",
+        date: new Date().toUTCString(),
+        "content-type": "application/json; charset=utf-8",
+      },
+      body: '{"error":"Request failed"}'
+    });
+  }
+);
+} catch (e) {
+  console.log('❌ Script Hub 兼容层捕获到原脚本未处理的错误')
+  if (_scriptSonverterCompatibilityType) {
+    console.log('⚠️ 故不修改本次' + (_scriptSonverterCompatibilityType === 'response' ? '响应' : '请求'))
+  } else {
+    console.log('⚠️ 因类型非请求或响应, 抛出错误')
+  }
+  console.log(e)
+  if (_scriptSonverterCompatibilityType) {
+    _scriptSonverterCompatibilityDone({})
+  } else {
+    throw e
+  }
+  }
